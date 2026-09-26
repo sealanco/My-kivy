a1a2cc80a67ea8822b2e9e5e313f46221d19408a
Sérgio <sealanco@gmail.com>
2026-09-26 16:46:00 +0100

    Fix text formatting in HelloWorldApp label

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ class HelloWorldApp(App):
     def build(self):
         # Returns a Label widget with the Hello World text and a larger font size
         return Label(
-            text="Olá mãe!\nDaqui é o Sérginho\na escrever o seu primeiro\nprograma.",
+            text="Olá mãe!\nDaqui é o Sérginho\na escrever o seu\nprimeiro programa.",
+            halign="center",
+            valign="middle",
             font_size='32sp'
         )
-
+label.bind(size=label.setter("text_size"))
 if __name__ == '__main__':
     HelloWorldApp().run()
